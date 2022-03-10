3caa8f0c4bc196c4c31881d94e0ebdc2bf8ff0ca
Guilherme Gazzo <guilhermegazzo@gmail.com>
2022-03-10 15:27:47 -0300

    Review

--- app/authorization/client/hasRole.ts
+++ app/authorization/client/hasRole.ts
@@ -1,6 +1,5 @@
 import { Roles } from '../../models/server/raw';
 
 export const hasRole = (userId: string, roleNames: string | string[], scope: string): Promise<boolean> => {
-	roleNames = [].concat(roleNames as []);
-	return Roles.isUserInRoles(userId, roleNames, scope);
+	return Roles.isUserInRoles(userId, [].concat(roleNames as []), scope);
 };
