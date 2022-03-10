2f9ba1a07b743abded2ba23bc15a3f502a2e40dd
Guilherme Gazzo <guilhermegazzo@gmail.com>
2022-03-10 16:01:38 -0300

    fix scope optional

--- app/authorization/server/functions/hasPermission.ts
+++ app/authorization/server/functions/hasPermission.ts
@@ -1,15 +1,15 @@
 import { Authorization } from '../../../../server/sdk';
 
-export const hasAllPermissionAsync = async (userId: string, permissions: string[], scope: string): Promise<boolean> =>
+export const hasAllPermissionAsync = async (userId: string, permissions: string[], scope?: string): Promise<boolean> =>
 	Authorization.hasAllPermission(userId, permissions, scope);
-export const hasPermissionAsync = async (userId: string, permissionId: string, scope: string): Promise<boolean> =>
+export const hasPermissionAsync = async (userId: string, permissionId: string, scope?: string): Promise<boolean> =>
 	Authorization.hasPermission(userId, permissionId, scope);
-export const hasAtLeastOnePermissionAsync = async (userId: string, permissions: string[], scope: string): Promise<boolean> =>
+export const hasAtLeastOnePermissionAsync = async (userId: string, permissions: string[], scope?: string): Promise<boolean> =>
 	Authorization.hasAtLeastOnePermission(userId, permissions, scope);
 
-export const hasAllPermission = (userId: string, permissions: string[], scope: string): boolean =>
+export const hasAllPermission = (userId: string, permissions: string[], scope?: string): boolean =>
 	Promise.await(hasAllPermissionAsync(userId, permissions, scope));
-export const hasPermission = (userId: string, permissionId: string, scope: string): boolean =>
+export const hasPermission = (userId: string, permissionId: string, scope?: string): boolean =>
 	Promise.await(hasPermissionAsync(userId, permissionId, scope));
-export const hasAtLeastOnePermission = (userId: string, permissions: string[], scope: string): boolean =>
+export const hasAtLeastOnePermission = (userId: string, permissions: string[], scope?: string): boolean =>
 	Promise.await(hasAtLeastOnePermissionAsync(userId, permissions, scope));
