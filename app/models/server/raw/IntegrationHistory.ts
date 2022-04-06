f065067a3bc452c8c97477963b843de2e72062f1
Aaron Ogle <aaron@geekgonecrazy.com>
2022-04-06 16:55:34 -0500

    call index creation directly

--- app/models/server/raw/IntegrationHistory.ts
+++ app/models/server/raw/IntegrationHistory.ts
@@ -1,8 +1,17 @@
+import { Collection } from 'mongodb';
 import { BaseRaw, IndexSpecification } from './BaseRaw';
 import { IIntegrationHistory } from '../../../../definition/IIntegrationHistory';
 
+type T = IIntegrationHistory;
+
 export class IntegrationHistoryRaw extends BaseRaw<IIntegrationHistory> {
 	protected indexes: IndexSpecification[] = [{ key: { _updatedAt: 1 }, expireAfterSeconds: 30 * 24 * 60 * 60 }]; // 30 days
+
+	constructor(public readonly col: Collection<T>, trash?: Collection<T>) {
+		super(col, trash);
+
+		this.col.createIndexes(this.indexes);
+	}
 
 	removeByIntegrationId(integrationId: string): ReturnType<BaseRaw<IIntegrationHistory>['deleteMany']> {
 		return this.deleteMany({ 'integration._id': integrationId });
